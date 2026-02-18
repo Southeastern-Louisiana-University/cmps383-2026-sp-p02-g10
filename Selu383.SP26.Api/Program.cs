d6e55cbc37730e64b8eec7a7cd22037eb33574f3
Eliora <181532355+ElioraBrowning@users.noreply.github.com>
2026-02-18 13:25:54 -0600

    React Web Published
    I've completed the setup steps from the doc

--- Selu383.SP26.Api/Program.cs
+++ Selu383.SP26.Api/Program.cs
@@ -39,9 +39,26 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app
+    .UseRouting()
+    .UseAuthentication()
+    .UseAuthorization()
+    .UseEndpoints(x =>
+    {
+        x.MapControllers();
+    });
 
-app.MapControllers();
+app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSpa(x =>
+    {
+        x.UseProxyToSpaDevelopmentServer("http://localhost:5173");
+    });
+} else {
+    app.MapFallbackToFile("index.html");
+}
 
 app.Run();
 
